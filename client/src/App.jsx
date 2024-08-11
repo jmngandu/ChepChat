f129fca10f3185b329489ad7c6c530b5374897b8
jmngandu <jamesmngandu@gmail.com>
2024-08-11 20:37:53 +0300

    chepchat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import "./styles.scss";
-import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-      <div className="chepchat">chepchat</div>
-      <Register />
+      <div className="chepchat">
+        <div className="sidebar">sidebar</div>
+        <div className="chatbox">chatbox</div>
+      </div>
     </>
   );
 }
