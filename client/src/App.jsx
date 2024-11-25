3e1ed242d20934540c0f45eb12791a192257c651
jmngandu <jamesmngandu@gmail.com>
2024-11-25 22:59:24 +0300

    chep chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./styles.scss";
 
 function App() {
   return (
     <>
       <div className="chepchat">
-        <div className="sidebar">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quasi
-          minus maiores repudiandae sunt eius maxime molestiae numquam quae,
-          eveniet cum doloribus, officia reiciendis accusantium est possimus
-          tempore suscipit illo!
-        </div>
+        <div className="sidebar"></div>
         <div className="chatbox">lorem</div>
       </div>
     </>
