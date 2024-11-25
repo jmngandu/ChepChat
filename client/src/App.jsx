c1b82aea9850c6e0b29ad4cee752b4e84f61e21d
jmngandu <jamesmngandu@gmail.com>
2024-11-26 00:14:17 +0300

    chepchat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,13 @@
-import { db } from "../firebase";
-import {
-  collection,
-  doc,
-  setDoc,
-  getDocs,
-  deleteDoc,
-} from "firebase/firestore";
 import "./styles.scss";
-import { useEffect } from "react";
+
 import Chatbox from "./components/chatbox/chat";
 
 function App() {
-  let students = [];
-  useEffect(() => {
-    async function getData() {
-      const readTheData = await getDocs(collection(db, "students"));
-      readTheData.forEach((doc) => {
-        students.push(doc.data());
-      });
-    }
-    getData();
-  }, []);
-
-  console.log(students);
-  async function addData() {
-    await setDoc(doc(db, "students", "7"), {
-      name: "Bob li",
-      course: "Software Enginerring",
-      age: 22,
-      year: 4,
-    });
-  }
-  async function deleteData() {
-    await deleteDoc(doc(db, "students", "7"));
-  }
   return (
     <>
       <div className="chepchat">
         <div className="sidebar">
-          <button onClick={addData}>add data</button>
-          <button onClick={deleteData}>delete doc</button>
+          <h1>CHEP CHAT</h1>
         </div>
         <div className="chatbox">
           <Chatbox />
--- client/src/components/chatbox/chat.jsx
+++ client/src/components/chatbox/chat.jsx
@@ -1,7 +1,50 @@
+import { db } from "../../../firebase";
+import {
+  collection,
+  getDocs,
+  setDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import { useEffect } from "react";
 const Chatbox = () => {
+  let students = [];
+  useEffect(() => {
+    async function getData() {
+      const studeDoc = await getDocs(collection(db, "students"));
+      studeDoc.forEach((doc) => {
+        students.push(doc.data());
+      });
+    }
+    getData();
+  }, []);
+  console.log(students);
+  async function addData() {
+    await setDoc(doc(db, "students", "3"), {
+      name: "James mukuvi",
+      course: "computer science",
+      age: 21,
+      year: 3,
+    });
+  }
+  async function updateData() {
+    await setDoc(
+      doc(db, "students", "2"),
+      {
+        age: 21,
+      },
+      { merge: true }
+    );
+  }
+  async function deleteData() {
+    await deleteDoc(doc(db, "students", "3"));
+  }
   return (
     <>
       <h1>chatbox</h1>
+      <button onClick={addData}>Add data</button>
+      <button onClick={deleteData}>Delete data</button>
+      <button onClick={updateData}>Update data</button>
     </>
   );
 };
